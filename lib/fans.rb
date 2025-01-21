7386dd067cfc818cf4684c70e707b665b70f6467
c <0xe0ffffgmail.com>
2025-01-21 15:04:21 +0800

    cache html

--- lib/fans.rb
+++ lib/fans.rb
@@ -6,17 +6,18 @@ require 'nokogiri'
 
 module Fans
   class Fetcher
-    attr_reader :pages, :links
+    attr_reader :url_to_id, :url_to_html, :id_relations
   
     def initialize
-      @page_id = 0
-      @pages = Hash.new{|h, k| h[k] = (@page_id += 1)}
-      @links = Hash.new{|h, from| h[from] = Hash.new{|h, to| h[to] = Set.new}}
+      @url_id = 0
+      @url_to_id = Hash.new{|h, k| h[k] = (@url_id += 1)}
+      @url_to_html = Hash.new{|h, k| h[k] = HTTP.get(k).body.to_s}
+      @id_relations = Hash.new{|h, from| h[from] = Hash.new{|h, to| h[to] = Set.new}}
     end
   
     def page_by_id(id)
-      if !@id_to_page || @id_to_page.size != pages.size
-        @id_to_page = pages.invert
+      if !@id_to_page || @id_to_page.size != url_to_id.size
+        @id_to_page = url_to_id.invert
       end
       @id_to_page[id]
     end
@@ -25,18 +26,17 @@ module Fans
       uri = URI.parse(url)
       host = "#{uri.scheme}://#{uri.host}"
   
-      from_id = pages[url]
-      resp = HTTP.get(url)
-      html = Nokogiri::HTML(resp.body.to_s)
+      from_id = url_to_id[url]
+      html = url_to_html[url]
   
-      html.css('a').each do |a|
+      Nokogiri::HTML(html).css('a').each do |a|
         to_page = a.attributes['href']&.value
         next unless to_page
         to_page = File.join(host, to_page) if to_page.start_with?('/')
-        to_id = pages[to_page]
+        to_id = url_to_id[to_page]
         text = a.text.strip.gsub(/\s+/, "\s")
         text = a.attributes['title']&.value&.gsub(/\s+/, "\s") if text.empty?
-        links[from_id][to_id] << text
+        id_relations[from_id][to_id] << text
       end
     end
   end
